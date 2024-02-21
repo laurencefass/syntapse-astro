9c857cf988b260705b446b6e441661f7ec01b073
Laurence Fass <laurencefass@hotmail.com>
2024-02-21 18:56:08 +0100

    misc changes

--- src/components/react/PubSub.tsx
+++ src/components/react/PubSub.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { atom } from 'nanostores'
 import { useStore } from '@nanostores/react'
 import "./styles.css"
@@ -7,19 +7,27 @@ export const $counter = atom<number>(0);
 export const $name = atom<string>("");
 
 export const Subscriber = () => {
-    const count = useStore($counter);
-    const name = useStore($name);
+  const count = useStore($counter);
+  const name = useStore($name);
 
-    return <div className="bordered">
-      <h2>Subscriber</h2>
-      <h3>count = {count}</h3>
-      <h3>name= {name}</h3>
-    </div>
+  useEffect(() => {
+    console.log("Subscriber loaded");
+  }, [])
+
+  return <div className="bordered">
+    <h2>Subscriber</h2>
+    <h3>count = {count}</h3>
+    <h3>name= {name}</h3>
+  </div>
 }
 
 export const Publisher = () => {
   const count = useStore($counter);
   const name = useStore($name);
+
+  useEffect(() => {
+    console.log("Publisher loaded");
+  }, [])
 
   return <div>
     <h1>Shared state between React components using nanostores</h1>
@@ -29,8 +37,8 @@ export const Publisher = () => {
       {name}
       <div>
         <h2>count: {count}</h2>
-        <button onClick={()=> $counter.set(count + 1)}>Increment</button>
-        <button onClick={()=> $counter.set(count - 1)}>Decrement</button>
+        <button onClick={() => $counter.set(count + 1)}>Increment</button>
+        <button onClick={() => $counter.set(count - 1)}>Decrement</button>
       </div>
     </div>
   </div>
