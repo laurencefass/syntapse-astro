76addb09ca30ab5369e28b906c79d951dcce6793
Laurence Fass <laurencefass@hotmail.com>
2024-06-20 13:45:45 +0200

    added solid js integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-
 import qwikdev from "@qwikdev/astro";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev(), react()],
+  integrations: [qwikdev(), react(), solidJs()],
   output: "server",
   adapter: node({
     mode: "standalone"
