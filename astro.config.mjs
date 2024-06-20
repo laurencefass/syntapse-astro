6fae477db4086804e16af1c22987c4879ce55e41
Laurence Fass <laurencefass@hotmail.com>
2024-06-20 17:33:29 +0200

    added solid integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-import qwikdev from "@qwikdev/astro";
-
-import solidJs from "@astrojs/solid-js";
+import qwik from "@qwikdev/astro";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev(), react(), solidJs()],
+  integrations: [
+    qwik(),
+    react(),
+    solid(), 
+  ], 
   output: "server",
   adapter: node({
     mode: "standalone"
---
+++ src/components/solid/Counter.solid.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource solid-js */
+import { createSignal } from "solid-js";
+
+export const Counter = () => {
+  const [count, setCount] = createSignal(0);
+  console.log("rendering solid counter");
+  return (
+    <div>
+      <h1>Solid.js counter</h1>
+      <p>Count: {count()}</p>
+      <button onClick={() => setCount(c => c + 1)}>Increment</button>
+    </div>
+  );
+};
