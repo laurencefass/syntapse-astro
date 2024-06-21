c2943253a116b83331b2f3425cf5f363bbc5a6f4
Laurence Fass <laurencefass@hotmail.com>
2024-06-21 15:39:14 +0200

    small refactor

--- src/components/solid/Counter.solid.tsx
+++ src/components/solid/Counter.solid.tsx
@@ -6,7 +6,7 @@ export const Counter = () => {
   console.log("rendering solid counter");
   return (
     <div>
-      <h1>Solid.js counter</h1>
+      <h1>Solid.js counter component</h1>
       <p>Count: {count()}</p>
       <button onClick={() => setCount(c => c + 1)}>Increment</button>
     </div>
